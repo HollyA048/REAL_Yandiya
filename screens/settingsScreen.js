0a9b367e298b23f059f5cd37711b7e4e9468c4ac
aidan <aidan@DESKTOP-4SLRJLF>
2023-08-15 11:14:56 +0100

    refactored the screen names to use camelcase

--- screens/settingsScreen.js
+++ screens/settingsScreen.js
@@ -4,7 +4,7 @@ import {
   Text
 } from 'react-native';
 
-export function SettingsScreen({ navigation: { navigate } }) {
+export function settingsScreen({ navigation: { navigate } }) {
     return (
         <View>
             <Text>Add Commission</Text>
