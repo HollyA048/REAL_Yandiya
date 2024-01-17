53b56c6ad70d6cd6cffcc8eb7db6a9906a41c618
aidanjohnys <aidan.johnys@gmail.com>
2024-01-17 10:26:31 +0000

    check boxes working now

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -1,44 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 class Checkbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checked: props._checked,
-      text: props.text
-    };
-  }
-
-  onPress() {
-    this.setState({checked: !this.state.checked});
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.onPress.bind(this)}>
-          <Text style={styles.check_icon}>{this.state.checked ? '✅' : '❌'}</Text>
-        </TouchableOpacity>
-        <Text style={{paddingLeft: 9}}>
-          {this.state.text}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.check_icon}>{this.props._checked ? '✅' : '❌'}</Text>
+                <Text style={{paddingLeft: 9}}>
+                    {this.props.text}
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    backgroundColor: 'lightgrey',
-  },
-  check_icon: {
-    fontSize: 40
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        backgroundColor: 'lightgrey',
+    },
+    check_icon: {
+        fontSize: 40
+    }
 });
 
 export default Checkbox;
--- screens/CommissionScreen.js
+++ screens/CommissionScreen.js
@@ -1,178 +1,144 @@
 import {useEffect, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView, Pressable, Touchable, TouchableOpacity,
-} from 'react-native';
-import {useNavigation, useRoute} from '@react-navigation/native';
-import { styles } from '../stylesheets/commission_css'
+import {Button, ScrollView, Text, TextInput, TouchableOpacity, View,} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {styles} from '../stylesheets/commission_css'
 
 import TopHeader from '../components/Header';
 import Checkbox from '../components/Checkbox';
 import axios from "axios";
 
 export function CommissionScreen({ route, navigation: { navigate }}) {
-  let commission_id = route.params?.commission_id;
-  const navigation = useNavigation();
-  const [checklistData, setChecklistData] = useState([]);
-  const [checkboxes, setCheckBoxes] = useState([]);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  function goalInputHandler(enteredText) {
-    throw new Error("Not Implemented");
-  }
-
-  function addGoalHandler() {
-    throw new Error("Not Implemented");
-  }
-
-  const getChecklist = () => {
-    return new Promise((resolve, reject) => {
-      axios({
-        method: 'get',
-        url: 'http://188.39.66.240:9080/get_checklist.php',
-        responseType: 'json',
-        params: {
-          "com_id" : commission_id.toString()
-        },
-        headers: { "Content-Type": "multipart/form-data" }
-      })
-        .then(function (response) {
-          resolve(response);
-        })
-
-        .catch(function (error) {
-          console.log(error);
-          reject(error);
-        })
-    })
-  };
-  const fetchChecklist = () => {
-    getChecklist()
-      .then(function (response) {
-        if (response.data !== null) {
-          createCheckBoxes(response.data.checklists);
-          setChecklistData(response.data.checklists);
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching checklists:', error);
-      })
-  };
-
-  const submitChecklist = () => {
-    checklistData.forEach((value) => {
-      console.log(value);
-    });
-    console.log("done");
-    /*
-    let checkboxValues = [];
-    checkboxes.map((checkbox) => (
-      checkboxValues.push([...checkboxValues, checkbox.getChecked()])
-    ))
-
-    onSubmitChecklist()
-      .then(function(response) {
-        console.log(response); // debug
-        navigation.navigate("Home");
-      })
-      .catch(error => {
-        console.error("Error Submitting Checklists:", error);
-        navigation.navigate("Home");
-      })*/
-  }
-
-  const onSubmitChecklist = (checklists) => {
-    const formData = new FormData();
-    formData.append("com_id", commission_id.toString());
-    formData.append("checklist_data", JSON.stringify(checklists));
-
-    return new Promise((resolve, reject) => {
-      axios({
-        method: 'post',
-        url: 'http://188.39.66.240:9080/update_checklist.php',
-        responseType: 'json',
-        data: formData,
-        headers: { "Content-Type": "multipart/form-data" }
-      })
-        .then(function (response) {
-          resolve(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-          reject(error);
-        })
-    })
-  }
-
-  useEffect(() => {
-    fetchChecklist();
-  }, [route]);
-
-  const toggleCheckBox = (index, checked) => {
-    const updatedChecklist = [...checklistData];
-    updatedChecklist[index].checked = checked ? "1" : "0";
-    setChecklistData(updatedChecklist);
-    console.log("checklists: " + JSON.stringify(checklistData));
-  };
-
-  const createCheckBoxes = (checklistData) => {
-    let newCheckBoxes = checklistData.map((task, index) => (
-       <Checkbox
-       _checked={task.checked !== 0}
-       text={task.title}
-       />
-    ));
-    setCheckBoxes(newCheckBoxes);
-  }
-
-  const handleModifyCheckboxValue = (index) => {
-    console.log("handle modify checkbox value");
-    let temp = checklistData[index]
-    if (checklistData[index].checked === 0) {
-      temp.checked = 1;
-    } else {
-      temp.checked = 0;
+    let commission_id = route.params?.commission_id;
+    const navigation = useNavigation();
+    const [checklistData, setChecklistData] = useState([]);
+    function goalInputHandler() {
+        throw new Error("Not Implemented");
     }
-    checklistData[index] = temp;
-  }
 
-  return (
+    function addGoalHandler() {
+        throw new Error("Not Implemented");
+    }
 
-    <View style={styles.appContainer}>
-      <TopHeader />
-      <ScrollView>
-        <View style={styles.inputContainer}>
-          <Text>{commission_id}</Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Custom requirement here:"
-            onChangeText={goalInputHandler}
-          />
-          <Button title={"Refresh"} onPress={() => fetchChecklist() }/>
-          <Button
-            title="Add"
-            onPress={addGoalHandler}
-            disabled={isButtonDisabled} />
+    const checklistNetworkRequest = () => {
+        // runs network request to GET the checklist data
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'get',
+                url: 'http://188.39.66.240:9080/get_checklist.php',
+                responseType: 'json',
+                params: {
+                    "com_id" : commission_id.toString()
+                },
+                headers: { "Content-Type": "multipart/form-data" }
+            })
+                .then(function (response) {
+                    resolve(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    reject(error);
+                })
+        })
+    };
+    const getChecklist = () => {
+        // runs checklistNetworkRequest and sets that data
+        checklistNetworkRequest()
+            .then(function (response) {
+                if (response.data !== null) {
+                    setChecklistData(response.data.checklists);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching checklists:', error);
+            })
+    };
+
+    const submitChecklist = () => {
+        // submits checklist data to the server
+        submitNetworkRequest()
+          .then(function(response) {
+            console.log(response); // debug
+            navigation.navigate("Home");
+          })
+          .catch(error => {
+            console.error("Error Submitting Checklists:", error);
+            navigation.navigate("Home");
+          })
+    }
+
+    const submitNetworkRequest = () => {
+        // runs network request to SUBMIT checklist data
+        const formData = new FormData();
+        formData.append("com_id", commission_id.toString());
+        formData.append("checklist_data", JSON.stringify(checklistData));
+
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'post',
+                url: 'http://188.39.66.240:9080/update_checklist.php',
+                responseType: 'json',
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" }
+            })
+                .then(function (response) {
+                    resolve(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    reject(error);
+                })
+        })
+    }
+
+    useEffect(() => {
+        getChecklist();
+    }, [route]);
+
+    const toggleCheckBox = (index, checked) => {
+        console.log(checked);
+        setChecklistData(prevChecklistData => {
+            let updatedChecklist = [...prevChecklistData];
+            updatedChecklist[index].checked = checked ? 0 : 1;
+            return updatedChecklist;
+        });
+    };
+
+    return (
+
+        <View style={styles.appContainer}>
+            <TopHeader />
+            <ScrollView>
+                <View style={styles.inputContainer}>
+                    <Text>{commission_id}</Text>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="Custom requirement here:"
+                        onChangeText={goalInputHandler}
+                    />
+                    <Button title={"Refresh"} onPress={() => getChecklist() }/>
+                    <Button
+                        title="Add"
+                        onPress={addGoalHandler}
+                    />
+                </View>
+                {checklistData === undefined ? (
+                    <View>
+                        <Text>No Checklists Found!</Text>
+                    </View>
+                ) : (
+                    checklistData.map((checkbox, index) => (
+                        <View style={styles.boxContainer} key={index}>
+                            <View style={styles.checkboxContainer}>
+                                <TouchableOpacity onPress={() => toggleCheckBox(index, checkbox.checked !== 0)}>
+                                    <Checkbox _checked={checkbox.checked !== 0} text={checkbox.title}/>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.checkBoxDescContainer}>
+                            </View>
+                        </View>
+                    )))}
+            </ScrollView>
+            <Button title={"Save"} onPress={() => submitChecklist(checklistData)}/>
         </View>
-        {checklistData === undefined ? (
-            <View>
-              <Text>No Checklists Found!</Text>
-            </View>
-        ) : (
-        checkboxes.map((checkbox, index) => (
-          <View style={styles.boxContainer} key={index}>
-            <View style={styles.checkboxContainer}>
-                {checkbox}
-            </View>
-            <View style={styles.checkBoxDescContainer}>
-            </View>
-          </View>
-      )))}
-      </ScrollView>
-      <Button title={"Save"} onPress={() => submitChecklist(checklistData)}/>
-    </View>
-  );
+    );
 }
