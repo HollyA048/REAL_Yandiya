280b3ee26605cafd61cd87e4d700ebd216deba31
ErtyPerty <115721409+ErtyPerty@users.noreply.github.com>
2024-01-17 13:48:24 +0000

    Added styles AddCommissionScreen.js ~ brendon
    temp fix for vertical styling, don't know if it works for horizontal

--- screens/AddCommissionScreen.js
+++ screens/AddCommissionScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Text, View, Button, TextInput} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import axios from "axios";
 import { styles } from '../stylesheets/AddCommission';
@@ -35,27 +35,77 @@ export function AddCommissionScreen({navigation: { navigate } }) {
     };
 
     return (
-        <View style={{top: "25%"}}>
-            <Button title={'Back'} onPress={() => navigate('Home')}/>
-            <Text style={styles.title2}>Yandiya Technologies LTD Commission Creating Tool.</Text>
+        <View style={{ height: '100%', display: 'flex' }}>
+            <Text style={styles.title2}>Create a Commission</Text>
             <View style={styles.entry}>
-                <Text style={styles.title1}>Title:</Text>
+                <Text style={styles.title1}>Title</Text>
                 <View style={styles.separator} />
                 <TextInput
                     placeholder="Enter Title"
                     value={title}
                     onChangeText={text => setTitle(text)}
+                    style={styles.input}
                 />
-                <Text style={styles.title1}>Description:</Text>
+            </View>
+            <View style={styles.entry}>
+                <Text style={styles.title1}>Description</Text>
                 <View style={styles.separator} />
                 <TextInput
                     placeholder="Enter Description"
                     value={description}
                     onChangeText={text => setDescription(text)}
+                    style={styles.input}
                 />
-                <Text>Status: {submitStatus}</Text>
-                <Button title="Submit" onPress={SubmitData}/>
+            </View>
+            <Text style={styles.status}>Status: {submitStatus}</Text>
+            <View style={styles.bottomButtons}>
+                <TouchableOpacity onPress={SubmitData}><Text style={styles.buttonText}>Submit</Text></TouchableOpacity>
+                <TouchableOpacity onPress={() => navigate('Home')}><Text style={styles.buttonText}>Back</Text></TouchableOpacity>
             </View>
         </View>
     )
 }
+
+const styles = {
+    title1: {
+        marginTop: '20%',
+        fontSize: 35,
+        fontWeight: 'bold',
+    },
+    title2: {
+        fontSize: 40,
+        marginTop: '30%',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    entry: {
+        padding: '10%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    separator: {
+        height: 3,
+        backgroundColor: '#e42c22',
+        width: 200,
+        marginTop: -2,
+        alignSelf: 'center',
+    },
+    status: {
+        textAlign: 'center',
+    },
+    input: {
+        fontSize: 30,
+    },
+    bottomButtons: {
+        width: '100%',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        top: '40%',
+    },
+    buttonText: {
+        color: 'blue',
+        fontSize: 35,
+        padding: 20
+    }
+};
